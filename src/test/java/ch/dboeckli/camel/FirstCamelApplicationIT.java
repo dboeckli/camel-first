31f8ba795ed60952531e92a9fb14ce8f92dff8b6
Dominique Boeckli <dboeckli@gmail.com>
2026-01-25 14:04:12 +0100

    improve tests

--- src/test/java/ch/dboeckli/camel/FirstCamelApplicationIT.java
+++ src/test/java/ch/dboeckli/camel/FirstCamelApplicationIT.java
@@ -1,5 +1,6 @@
 package ch.dboeckli.camel;
 
+import ch.dboeckli.camel.routes.*;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Route;
@@ -11,8 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(
     useMainMethod = SpringBootTest.UseMainMethod.ALWAYS,
@@ -37,8 +37,20 @@ class FirstCamelApplicationIT {
         assertNotNull(applicationContext, "Application context should not be null");
         log.info("Testing Spring 6 Application {}", applicationContext.getApplicationName());
 
-        List<Route> routes = camelContext.getRoutes();
-        assertEquals(7, routes.size());
+        List<String> routeIds = camelContext.getRoutes().stream()
+            .map(Route::getRouteId)
+            .toList();
+        log.info("Found Route IDs: {}", routeIds);
+
+        assertAll("Check all route IDs are present",
+            () -> assertEquals(7, routeIds.size()),
+            () -> assertTrue(routeIds.contains(ActiveMqSenderRouter.ACTIVE_MQ_ROUTER_ID)),
+            () -> assertTrue(routeIds.contains(MySecondTimerRouter.MY_SECOND_ROUTE_ID)),
+            () -> assertTrue(routeIds.contains(MyThirdTimerRouter.MY_THIRD_ROUTE_ID)),
+            () -> assertTrue(routeIds.contains(MyForthTimerRouter.MY_FORTH_ROUTE_ID)),
+            () -> assertTrue(routeIds.contains(MyFileRouter.LIST_FILES_ROUTE_ID)),
+            () -> assertTrue(routeIds.contains(MyFileRouter.COPY_FILES_ROUTE_ID))
+        );
     }
 
 
