91479c2149e50a6025ec01a5fa08c0309cadec30
Dominique Boeckli <dboeckli@gmail.com>
2026-06-11 13:24:36 +0200

    fixup 2

--- src/test/java/ch/dboeckli/camel/FirstCamelApplicationIT.java
+++ src/test/java/ch/dboeckli/camel/FirstCamelApplicationIT.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
         properties = { "otel.java.global-autoconfigure.enabled=true", "spring.docker.compose.enabled=true",
                 "spring.docker.compose.skip.in-tests=false", "spring.docker.compose.file=compose-with-mq.yaml" })
 @Slf4j
+@DirtiesContext
 @ActiveProfiles("local")
 class FirstCamelApplicationIT {
 
